d6a0cf45559c3995b13154a33d4e7e8184f6c8d6
Erik Hornberger <erik@eext.us>
2016-06-18 07:19:22 +0900

    iOS App Initial Commit

---
+++ RedBallTrackingiOS/RedBallTrackingiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedBallTrackingiOS
+//
+//  Created by Erik Hornberger on 2016/06/18.
+//  Copyright © 2016年 EExT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
